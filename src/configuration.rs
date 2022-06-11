d88ffd26d4f7e2b42fd8fd0f4a31d9d998ed9831
Markus Zehnder <markus.z@unfoldedcircle.com>
2022-06-11 15:09:34 +0200

    refactor: configuration handling refactoring
    - configuration file is now optional
    - default configuration file `configuration.yaml` is automatically loaded if present
    - optional cmd line parameter to override configuration file
    - fix cfg value override with ENV variables
    - determine APP & API versions during build

--- src/configuration.rs
+++ src/configuration.rs
@@ -10,35 +10,39 @@ use url::Url;
 
 const DEF_CONNECTION_TIMEOUT: u8 = 3;
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, serde::Serialize)]
 pub struct Settings {
-    pub webserver: WebServerSettings,
-    pub home_assistant: HomeAssistantSettings,
+    pub integration: IntegrationSettings,
+    pub hass: HomeAssistantSettings,
 }
 
-#[derive(serde::Deserialize)]
-pub struct WebServerSettings {
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct IntegrationSettings {
     pub interface: String,
-    pub http: bool,
-    pub http_port: u16,
-    pub https: bool,
-    pub https_port: u16,
+    pub http: WebServerSettings,
+    pub https: WebServerSettings,
     pub certs: Option<CertificateSettings>,
     pub websocket: Option<WebSocketSettings>,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct WebServerSettings {
+    pub enabled: bool,
+    pub port: u16,
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
 pub struct CertificateSettings {
     pub public: String,
     pub private: String,
 }
 
-#[derive(Default, serde::Deserialize)]
+#[derive(Default, serde::Deserialize, serde::Serialize)]
 pub struct WebSocketSettings {
     pub token: Option<String>,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, serde::Serialize)]
 pub struct HomeAssistantSettings {
     pub url: Url,
     pub token: String,
@@ -50,7 +54,7 @@ pub struct HomeAssistantSettings {
 }
 
 #[serde_as]
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, serde::Serialize)]
 pub struct ReconnectSettings {
     pub attempts: u16,
     #[serde_as(as = "DurationMilliSeconds")]
@@ -74,7 +78,7 @@ impl Default for ReconnectSettings {
 }
 
 #[serde_as]
-#[derive(Clone, serde::Deserialize)]
+#[derive(Clone, serde::Deserialize, serde::Serialize)]
 pub struct HeartbeatSettings {
     /// How often heartbeat pings are sent
     #[serde_as(as = "DurationSeconds")]
@@ -98,16 +102,20 @@ impl Default for HeartbeatSettings {
 impl Default for Settings {
     fn default() -> Settings {
         Settings {
-            webserver: WebServerSettings {
+            integration: IntegrationSettings {
                 interface: "0.0.0.0".to_string(),
-                http: false,
-                http_port: 8000,
-                https: true,
-                https_port: 8443,
+                http: WebServerSettings {
+                    enabled: true,
+                    port: 8000,
+                },
+                https: WebServerSettings {
+                    enabled: false, // TODO https should be the default, but not yet implemented
+                    port: 8443,
+                },
                 certs: None,
                 websocket: None,
             },
-            home_assistant: HomeAssistantSettings {
+            hass: HomeAssistantSettings {
                 url: Url::parse("ws://hassio.local:8123/api/websocket").unwrap(),
                 token: "".to_string(),
                 connection_timeout: DEF_CONNECTION_TIMEOUT,
@@ -119,36 +127,46 @@ impl Default for Settings {
     }
 }
 
-pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let config = Config::builder()
-        .add_source(config::File::with_name("configuration"))
-        // Add in settings from the environment (with a prefix of UC_HASS)
-        // Eg.. `UC_HASS_DEBUG=1 ./target/app` would set the `debug` key
-        .add_source(config::Environment::with_prefix("UC_HASS"))
+pub fn get_configuration(filename: Option<&str>) -> Result<Settings, config::ConfigError> {
+    // default configuration
+    let mut config = Config::builder().add_source(Config::try_from(&Settings::default())?);
+    // read optional configuration file to override defaults
+    if let Some(filename) = filename {
+        config = config.add_source(config::File::with_name(filename));
+    }
+    // Add in settings from the environment (with a prefix of UC)
+    // Eg.. `UC_HASS_URL=http://localhost:8123/api/websocket` would set the `hass.url` key
+    // This does NOT WORK for nested configurations! https://github.com/mehcode/config-rs/issues/312
+    let config = config
+        .add_source(config::Environment::with_prefix("UC").separator("_"))
         .build()?;
 
-    let mut settings: Settings = config.try_deserialize()?;
-    if settings.home_assistant.reconnect.backoff_factor < 1.0
-        || settings.home_assistant.reconnect.duration.as_millis() < 100
-        || settings.home_assistant.reconnect.duration_max.as_millis() < 1000
+    let settings: Settings = config.try_deserialize()?;
+
+    check_cfg_values(settings)
+}
+
+fn check_cfg_values(mut settings: Settings) -> Result<Settings, config::ConfigError> {
+    if settings.hass.reconnect.backoff_factor < 1.0
+        || settings.hass.reconnect.duration.as_millis() < 100
+        || settings.hass.reconnect.duration_max.as_millis() < 1000
     {
         warn!("Invalid HA reconnect settings, using defaults.");
-        settings.home_assistant.reconnect = Default::default();
+        settings.hass.reconnect = Default::default();
     }
 
-    if settings.home_assistant.heartbeat.interval.as_secs() < 5
-        || settings.home_assistant.heartbeat.timeout.as_secs() < 5
-        || settings.home_assistant.heartbeat.timeout.as_secs()
-            <= settings.home_assistant.heartbeat.interval.as_secs()
+    if settings.hass.heartbeat.interval.as_secs() < 5
+        || settings.hass.heartbeat.timeout.as_secs() < 5
+        || settings.hass.heartbeat.timeout.as_secs() <= settings.hass.heartbeat.interval.as_secs()
     {
         warn!("Invalid HA heartbeat settings, using defaults.");
-        settings.home_assistant.heartbeat = Default::default();
+        settings.hass.heartbeat = Default::default();
     }
 
-    match settings.home_assistant.url.scheme() {
+    match settings.hass.url.scheme() {
         "ws" | "wss" => {}
-        "http" => settings.home_assistant.url.set_scheme("ws").unwrap(),
-        "https" => settings.home_assistant.url.set_scheme("wss").unwrap(),
+        "http" => settings.hass.url.set_scheme("ws").unwrap(),
+        "https" => settings.hass.url.set_scheme("wss").unwrap(),
         scheme => {
             return Err(config::ConfigError::Message(format!(
                 "invalid scheme in home_assistant.url: {}. Valid: [ws, wss]",
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,14 @@
 
 use std::io;
 use std::net::TcpListener;
+use std::path::Path;
 
 use actix::Actor;
 use actix_web::{middleware, web, App, HttpServer};
-
+use clap::{arg, Command};
+use const_format::formatcp;
+use lazy_static::lazy_static;
+use log::{error, info};
 use server::json_error_handler;
 
 use crate::configuration::get_configuration;
@@ -25,16 +29,72 @@ mod server;
 mod util;
 mod websocket;
 
+pub const DEF_CONFIG_FILE: &str = "configuration.yaml";
+
+pub mod built_info {
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+}
+
+pub const APP_VERSION: &str = formatcp!(
+    "{}{}",
+    match built_info::GIT_VERSION {
+        Some(v) => v,
+        None => formatcp!("{}-non-git", built_info::PKG_VERSION),
+    },
+    match built_info::GIT_DIRTY {
+        Some(_) => "-dirty",
+        None => "",
+    }
+);
+
+lazy_static! {
+    pub static ref API_VERSION: &'static str = built_info::DEPENDENCIES
+        .iter()
+        .find(|p| p.0 == "uc_api")
+        .map(|v| v.1)
+        .unwrap_or("?");
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let args = Command::new(built_info::PKG_NAME)
+        .author("Unfolded Circle Aps")
+        .version(&*APP_VERSION)
+        .about("Home Assistant integration for Remote Two")
+        .arg(arg!(-c --config <FILE> ... "Configuration file").required(false))
+        .get_matches();
+
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
-    let cfg = get_configuration().expect("Failed to read configuration");
+    let cfg_file = match args.value_of("config") {
+        None => {
+            if Path::new(DEF_CONFIG_FILE).exists() {
+                info!("Loading default configuration file: {}", DEF_CONFIG_FILE);
+                Some(DEF_CONFIG_FILE)
+            } else {
+                None
+            }
+        }
+        Some(c) => Some(c),
+    };
+    let cfg = get_configuration(cfg_file).expect("Failed to read configuration");
 
-    let address = format!("{}:{}", cfg.webserver.interface, cfg.webserver.http_port);
-    let listener = Some(TcpListener::bind(address)?);
-    let listener_tls = if cfg.webserver.https {
-        let address = format!("{}:{}", cfg.webserver.interface, cfg.webserver.https_port);
+    let listener = if cfg.integration.http.enabled {
+        let address = format!(
+            "{}:{}",
+            cfg.integration.interface, cfg.integration.http.port
+        );
+        println!("{} listening on: {}", built_info::PKG_NAME, address);
+        Some(TcpListener::bind(address)?)
+    } else {
+        None
+    };
+    let listener_tls = if cfg.integration.https.enabled {
+        let address = format!(
+            "{}:{}",
+            cfg.integration.interface, cfg.integration.https.port
+        );
+        println!("{} listening on: {}", built_info::PKG_NAME, address);
         Some(TcpListener::bind(address)?)
     } else {
         None
@@ -47,8 +107,8 @@ async fn main() -> std::io::Result<()> {
         ));
     }
 
-    let websocket_settings = web::Data::new(cfg.webserver.websocket.unwrap_or_default());
-    let controller = web::Data::new(Controller::new(cfg.home_assistant).start());
+    let websocket_settings = web::Data::new(cfg.integration.websocket.unwrap_or_default());
+    let controller = web::Data::new(Controller::new(cfg.hass).start());
 
     let mut http_server = HttpServer::new(move || {
         App::new()
@@ -65,10 +125,12 @@ async fn main() -> std::io::Result<()> {
     })
     .workers(1);
 
-    // if listener_tls.is_some() {
-    //     let server_cfg = load_ssl(&settings.webserver.certs)?;
-    //     http_server = http_server.listen_rustls(listener_tls.unwrap(), server_cfg)?;
-    // }
+    if listener_tls.is_some() {
+        // let server_cfg = load_ssl(&settings.webserver.certs)?;
+        //http_server = http_server.listen_rustls(listener_tls.unwrap(), server_cfg)?;
+        error!("TODO certificate handling not yet implemented. Please use http only. Sorry.");
+        std::process::exit(1);
+    }
 
     if listener.is_some() {
         http_server = http_server.listen(listener.unwrap())?;
